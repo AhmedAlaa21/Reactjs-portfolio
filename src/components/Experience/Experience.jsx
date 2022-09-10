3c514a28d3d064e958a46b6f8bf31a9edc0dd2fc
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2022-09-10 12:39:46 +0200

    update content

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,103 @@
 import React from "react";
 import "./Experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
-  return <section id="exp">Experience</section>;
+  return (
+    <section id="exp">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container exp_container">
+        <div className="exp_frontend">
+          <h3>Frontend Development</h3>
+          <div className="exp_content">
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="exp_backend">
+          <h3>Backend Development</h3>
+          <div className="exp_content">
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Experienced</small>{" "}
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>MongoDB</h4>
+
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="exp_details">
+              <BsPatchCheckFill className="exp_icon"/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,77 @@
 import React from "react";
 import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
 const Portfolio = () => {
-  return <section id="porfolio">Portfolio</section>;
+  const portfolioProjects = [
+    {
+      id: "1",
+      imgPath: IMG1,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+    {
+      id: "2",
+      imgPath: IMG2,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+    {
+      id: "3",
+      imgPath: IMG3,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+    {
+      id: "4",
+      imgPath: IMG4,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+    {
+      id: "5",
+      imgPath: IMG5,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+    {
+      id: "6",
+      imgPath: IMG6,
+      title: "This is a portfolio item title",
+      // TODO add links
+    },
+  ];
+  return (
+    <section id="porfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        {portfolioProjects.map((portfolioProject) => {
+          return (
+            <article className="portfolio_item" key={portfolioProject.id}>
+              <div className="portfolio_item-image">
+                <img src={portfolioProject.imgPath} alt="" />
+              </div>
+              <h3>{portfolioProject.title}</h3>
+              <div className="portfolio_item-cta">
+                <a href="" className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href="" className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
