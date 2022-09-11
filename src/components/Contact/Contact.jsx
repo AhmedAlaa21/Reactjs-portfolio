d9b8f0a5c68a3f0b82c9e0d64b4fe40df90b9a12
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2022-09-11 13:39:28 +0200

    update content

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_t93aflo",
+        "template_idtgpjv",
+        form.current,
+        "JCAYX4qWECMp5ULYA"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if(result.status === 200) notify();
+          else notify2()
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          notify2();
+        }
+      );
+  };
+
+  const notify = () => {
+    toast.success("We received your message, Thank you", {
+      position: toast.POSITION.BOTTOM_CENTER
+    });  
+  }
+  const notify2 = () => {
+    toast.error("Error occured please try again!", {
+      position: toast.POSITION.BOTTOM_CENTER
+    });
+  }
+
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_option-icon" />
+            <h4>Email</h4>
+            <h5>ahmedmohamedalaa96@gmail.com</h5>
+            <a
+              href="mailto:ahmedmohamedalaa96@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine className="contact_option-icon" />
+            <h4>Messenger</h4>
+            <h5>Ahmed Mohamed Alaa</h5>
+            <a
+              href="https://m.me/ahmed.mohamed.alaa2016"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+20 114-224-5739</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=01142245739"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/* end of contact options */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary submit-btn">
+            Send Message
+          </button>
+        </form>
+      </div>
+     <ToastContainer />
+    </section>
+  );
 };
 
 export default Contact;
