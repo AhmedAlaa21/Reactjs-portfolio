44f67b4f08acb6c9ae2e6063a592b99c3c3174d5
ahmed.alaa3 <ahmed.alaa@badgewell.com>
2023-05-04 20:08:10 +0200

    update cv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import CTA from "./CTA";
 import "./Header.css";
-import Me from '../../assets/me.png';
+import Me from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <header id="start">
-    <div className="container header_container">
-      <h5>Hello I'm</h5>
-      <h1>Ahmed Mohamed Alaa</h1>
-      <h5 className="text-light"> React.js Developer</h5>
-      <CTA />
-      <HeaderSocials />
-      <div className="me">
-        <img src={Me} alt="formal" />
+  return (
+    <header id="start">
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Ahmed Mohamed Alaa</h1>
+        <h5 className="text-light"> React Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={Me} alt="formal" />
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
-      <a href="#contact" className="scroll_down">Scroll Down</a>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
