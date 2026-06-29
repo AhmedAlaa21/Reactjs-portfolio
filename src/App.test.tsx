8ff59a5100395a0ed2dc7cf3d9b4653275f5caf8
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2026-06-29 16:47:54 +0300

    add .npmrc for legacy peer dependencies, update package.json and package-lock.json with new devDependencies and dependency versions, change module resolution in tsconfig.json, and refactor App.test.tsx for cleaner rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders portfolio header", () => {
-  render(<App />);
-  const nameElement = screen.getByText(/Ahmed Mohamed Alaa/i);
-  expect(nameElement).toBeInTheDocument();
+  const { getByText } = render(<App />);
+  expect(getByText(/Ahmed Mohamed Alaa/i)).toBeInTheDocument();
 });
